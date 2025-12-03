ea9106fafe4a145cfef9b0517c401b6512b3d53c
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2025-12-03 21:02:35 +0530

    Add CartTotal component and integrate it into Cart page; enhance ShopContext with getCartCount and getCartAmount functions

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from 'react';
 import { products } from '../assets/assets.js';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export const ShopContext = createContext();
 
@@ -35,6 +36,49 @@ const ShopContextProvider = ({ children }) => {
     setCartItems(cartData);
   };
 
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try 
+          {if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+
+        }
+      }
+    }
+    return totalCount;
+  }
+
+  const updateQuantity = async (itemId, size, quantity) => {
+
+    let cartData = structuredClone(cartItems);
+
+    cartData[itemId][size] = quantity;
+
+    setCartItems(cartData);
+  }
+
+  const getCartAmount = () => {
+    let totalAmount  = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find(( product ) => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalAmount;
+  };
+
+
   const value = {
     products,
     currency,
@@ -45,7 +89,10 @@ const ShopContextProvider = ({ children }) => {
     setShowSearch,
     cartItems, 
     setCartItems,
-    addToCart
+    addToCart,
+    getCartCount,
+    updateQuantity,
+    getCartAmount,
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
